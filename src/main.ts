0561ac61284112def538cf5dae9a7b6c732be3ef
Erik Bigler <erikbigler@gmail.com>
2021-08-25 21:03:41 -0500

    implement $roundDecimal variable

--- src/main.ts
+++ src/main.ts
@@ -7,26 +7,43 @@ interface Params {
 const script: Firebot.CustomScript<Params> = {
   getScriptManifest: () => {
     return {
-      name: "Starter Custom Script",
-      description: "A starter custom script for build",
-      author: "SomeDev",
+      name: "$roundDecimal Variable",
+      description: "Adds a $roundDecimal variable",
+      author: "ebiggz",
       version: "1.0",
       firebotVersion: "5",
+      startupOnly: true,
     };
   },
-  getDefaultParameters: () => {
-    return {
-      message: {
-        type: "string",
-        default: "Hello World!",
-        description: "Message",
-        secondaryDescription: "Enter a message here",
-      },
-    };
-  },
+  getDefaultParameters: null,
   run: (runRequest) => {
-    const { logger } = runRequest.modules;
-    logger.info(runRequest.parameters.message);
+    const { replaceVariableManager } = runRequest.modules;
+    replaceVariableManager.registerReplaceVariable({
+      definition: {
+        handle: "roundDecimal",
+        description: "Rounds the given number to the nearest whole number.",
+        usage: "roundDecimal[num, decimalPlaces]",
+        examples: [
+          {
+            usage: "roundDecimal[0.12345, 2]",
+            description: "Round to two decimal places."
+          }
+        ],
+        possibleDataOutput: ["number"],
+      },
+      evaluator: (_, number, decimalPlaces = 2) => {
+
+        if (isNaN(number)) {
+          return 0;
+        }
+
+        if (isNaN(decimalPlaces)) {
+          return number;
+        }
+
+        return Number(number).toFixed(parseInt(decimalPlaces, 10));
+      },
+    });
   },
 };
 
